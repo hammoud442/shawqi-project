be898b72b349cdaf3bef25940aef7c8f75a8df11
Deema <deema@Deemas-MacBook-Pro.local>
2024-10-27 19:04:44 +0200

    commit readme

---
+++ project-04/index.js
@@ -0,0 +1,19 @@
+// commit
+
+let name = prompt("What is your name?");
+
+let age = prompt("What is your age?");
+
+let height = prompt("What is your height?");
+
+let weight = prompt("What is your weight?");
+
+let gender = prompt("What is your gender?");
+
+console.log(
+  `Hello ${name}, your age is ${age} and your height is ${height} and your weight is ${weight} and your gender is ${gender}`
+);
+
+document.write(
+  `<h1>Hello ${name}, your age is ${age} and your height is ${height} and your weight is ${weight} and your gender is ${gender}</h1>`
+);
